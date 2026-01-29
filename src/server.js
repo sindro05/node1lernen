c00c11fbd77fca133075a47b0e017c5b4e73b57d
sindrot05 <sindrot05@gmail.com>
2026-01-29 18:31:10 +0300

    connection db

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+import db from "./config/db.js";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+db.getConnection()
+  .then(() => console.log("✅ Database connected"))
+  .catch(err => console.error("DB error:", err));
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
